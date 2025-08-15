2356452ec44aeb503ec4861651ca228611ca1785
EZ <31062364+EagleZhen@users.noreply.github.com>
2025-08-15 13:03:39 -0700

    update feedback button spacing

--- web/src/components/FeedbackButton.tsx
+++ web/src/components/FeedbackButton.tsx
@@ -33,7 +33,7 @@ export default function FeedbackButton() {
         size="lg"
         title="Share feedback about this course planner"
       >
-        <MessageCircle className="w-5 h-5 mr-2" />
+        <MessageCircle className="w-5 h-5" />
         <span className="hidden sm:inline">Feedback</span>
       </Button>
       
