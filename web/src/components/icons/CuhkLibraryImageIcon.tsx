938562d84d92c8e121dbead0169b96f8d6234df4
EZ <31062364+EagleZhen@users.noreply.github.com>
2026-05-18 16:15:15 +0800

    refactor: use `Image` component for better image optimization

--- web/src/components/icons/CuhkLibraryImageIcon.tsx
+++ web/src/components/icons/CuhkLibraryImageIcon.tsx
@@ -1,14 +1,19 @@
+import Image from 'next/image'
+
 interface CuhkLibraryImageIconProps {
   className?: string
 }
 
 export function CuhkLibraryImageIcon({ className }: CuhkLibraryImageIconProps) {
   return (
-    <img
+    <Image
       className={className}
       src="/icons/cuhk-library-small.png"
+      width={30}
+      height={30}
       alt=""
       aria-hidden="true"
+      unoptimized
     />
   )
 }
