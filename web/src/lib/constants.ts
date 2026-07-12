ebca41171581c258a69475edc4f52d56ba08f160
EZ <31062364+EagleZhen@users.noreply.github.com>
2026-07-12 17:41:17 +0800

    chore(web): bump notice version to re-show redesigned notice

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -5,7 +5,7 @@
 // Mobile notice configuration
 export const MOBILE_BREAKPOINT = 768 // Tailwind md breakpoint
 export const NOTICE_STORAGE_KEY = 'desktop-notice-version'
-export const NOTICE_VERSION = '1' // Bump to re-show notice to all users
+export const NOTICE_VERSION = '2' // Bump to re-show notice to all users
 export const NOTICE_IMAGE_LOADED_EVENT = 'mobile-notice-image-loaded'
 
 // Data versioning
